0c1efc691d92335750b2a7254568cfa5dcc639c3
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-09-06 11:16:41 +0100

    setup tinymc

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import { Container, Form, Input, Button, Col, Row } from 'reactstrap';
+import { Editor } from '@tinymce/tinymce-react';
+// // Import TinyMCE
+// import tinymce from 'tinymce/tinymce';
+
+// // A theme is also required
+// import 'tinymce/themes/silver';
+
+// // Any plugins you want to use has to be imported
+// import 'tinymce/plugins/paste';
+// import 'tinymce/plugins/link';
 import {
   getSpellingAndGrammar,
   getBias,
   structure
 } from '../../utils/checkDescription';
+require.context(
+  'file-loader?name=[path][name].[ext]&context=node_modules/tinymce!tinymce/skins',
+  true,
+  /.*/
+);
+// // Initialize the app
+// tinymce.init({
+//   selector: 'tiny-mc',
+//   plugins: ['paste', 'link']
+// });
+
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -15,6 +36,10 @@ class Main extends Component {
       bias: null
     };
   }
+
+  handleEditorChange = e => {
+    console.log('Content was updated:', e.target.getContent());
+  };
   onFormSubmit = async e => {
     e.preventDefault();
     const { html } = this.state;
@@ -45,6 +70,31 @@ class Main extends Component {
             </Form>
           </Col>
           <Col>PlaceHolder</Col>
+        </Row>
+        <Row>
+          <Col>
+            <Editor
+              id="tiny-mc"
+              initialValue="<p>This is the initial content of the editor</p>"
+              init={{
+                // selector: 'tiny-mc',
+                height: 500,
+                menubar: false,
+                plugins: [
+                  'advlist autolink lists link image charmap print preview anchor',
+                  'searchreplace visualblocks code fullscreen',
+                  'insertdatetime media table paste code help wordcount'
+                ],
+                toolbar:
+                  'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+                content_css: [
+                  '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                  '//www.tiny.cloud/css/codepen.min.css'
+                ]
+              }}
+              onChange={this.handleEditorChange}
+            />
+          </Col>
         </Row>
       </Container>
     );
